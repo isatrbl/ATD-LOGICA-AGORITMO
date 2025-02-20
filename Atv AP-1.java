af7a265ea4e67c6965b527edbbf0131a012e6d24
isatrbl <isabellagomestavares01@gmail.com>
2025-02-20 11:22:49 -0300

    Update Atv AP-1.java

--- Atv AP-1.java
+++ Atv AP-1.java
@@ -17,4 +17,28 @@ public class Main {
         }
         
         scanner.close();
+
+
+package exercicio1;
+
+import java.util.Scanner;
+public class TabuadaWhile {
+	  public static void main(String[] args) {
+		  Scanner scan = new Scanner.nextInt(System.in);
+		            System.out.print("Digite um número maior que 1: ");
+		           int numero= scan.nextInt();
+		            if (numero > 1) {
+		                int i = 1;  
+		                while (i <= 10) {
+		                    System.out.println(numero + " x " + i + " = " + (numero * i));
+		                    i++;  
+		                }
+		            } else {
+		                System.out.println("O número precisa ser maior que 1.");
+		            }
+		        } catch (Exception e) {
+		            System.out.println("Por favor, digite um número válido.");
+		        }
+		        
+		        scanner
         
